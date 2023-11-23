8f10aea5187db65fb0cce2127322e2256c018056
Nathan LORIT <nathan.lorit@viacesi.fr>
2023-11-23 11:41:16 +0100

    updated navbar and footer

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './app/components/Header/Header';
 import Navbar from './app/components/Navbar/Navbar';
+import Footer from './app/components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
+      <Footer/>
     </div>
   );
 }
