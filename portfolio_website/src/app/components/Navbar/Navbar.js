8259e6df11f44ed7f025f3fe555758ae717e5e7e
Nathan LORIT <nathan.lorit@viacesi.fr>
2023-11-23 10:47:50 +0100

    Modif Homepage

---
+++ portfolio_website/src/app/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png?20171228163613"></img>
+                <div class="topnav">
+                    <a class="active" href="#home">Home</a>
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+                    <a href="#about">About</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
