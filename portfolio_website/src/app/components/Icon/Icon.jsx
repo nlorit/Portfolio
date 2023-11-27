f9e4ca81a698b09c157e1274180236a1948f77a5
Nathan LORIT <nathan.lorit@viacesi.fr>
2023-11-27 15:01:37 +0100

    add new icon

---
+++ portfolio_website/src/app/components/Icon/Icon.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      data-name="Calque 1"
+      viewBox="0 0 413.82 403.65"
+    >
+      <defs>
+        <clipPath id="clippath">
+          <path fill="none" strokeWidth="0" d="M0 0H413.82V403.65H0z"></path>
+        </clipPath>
+        <clipPath id="clippath-1">
+          <path fill="none" strokeWidth="0" d="M0 0H413.82V403.65H0z"></path>
+        </clipPath>
+        <clipPath id="clippath-2">
+          <path fill="none" strokeWidth="0" d="M0 0H413.82V403.65H0z"></path>
+        </clipPath>
+        <clipPath id="clippath-3">
+          <path fill="none" strokeWidth="0" d="M0 0H413.82V403.65H0z"></path>
+        </clipPath>
+      </defs>
+      <path
+        fill="#a2c956"
+        strokeWidth="0"
+        d="M351.87 65.36L410.87 95.36 411.1 187.36 351.65 218.36 351.87 65.36z"
+      ></path>
+      <g clipPath="url(#clippath)">
+        <path
+          fill="none"
+          stroke="#292627"
+          strokeWidth="3"
+          d="M351.87 65.36L410.87 95.36 411.1 187.36 351.65 218.36 351.87 65.36z"
+        ></path>
+      </g>
+      <path
+        fill="#040505"
+        strokeWidth="0"
+        d="M235.58 4.36L294.58 34.36 294.58 308.36 411.58 247.36 412.31 309.19 235.36 400.36 235.58 4.36z"
+      ></path>
+      <g clipPath="url(#clippath-1)">
+        <path
+          fill="none"
+          stroke="#fff"
+          strokeWidth="3"
+          d="M235.58 4.36L294.58 34.36 294.58 308.36 411.58 247.36 412.31 309.19 235.36 400.36 235.58 4.36z"
+        ></path>
+      </g>
+      <path
+        fill="#a2c956"
+        strokeWidth="0"
+        d="M118.58 247.36L177.58 278.36 177.82 401.18 118.35 370.36 118.58 247.36z"
+      ></path>
+      <g clipPath="url(#clippath-2)">
+        <path
+          fill="none"
+          stroke="#292627"
+          strokeWidth="3"
+          d="M118.58 247.36L177.58 278.36 177.82 401.18 118.35 370.36 118.58 247.36z"
+        ></path>
+      </g>
+      <path
+        fill="#040505"
+        strokeWidth="0"
+        d="M179.46 2.48L179.46 218.12 60.73 157.3 60.5 339.3 1.73 309.3 1.5 94.3 37.73 78.3 119 106.3 119 34.3 179.46 2.48z"
+      ></path>
+      <g clipPath="url(#clippath-3)">
+        <path
+          fill="none"
+          stroke="#fff"
+          strokeWidth="3"
+          d="M179.46 2.48L179.46 218.12 60.73 157.3 60.5 339.3 1.73 309.3 1.5 94.3 37.73 78.3 119 106.3 119 34.3 179.46 2.48z"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default Icon;
