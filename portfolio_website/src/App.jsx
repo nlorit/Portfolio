0d6dd44e83c470ab7f6f7e544ab745900866e801
Nathan LORIT <nathan.lorit@viacesi.fr>
2023-12-15 12:26:28 +0100

    Optimized with copilot

--- portfolio_website/src/App.jsx
+++ portfolio_website/src/App.jsx
@@ -1,18 +1,21 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './app/components/Home/Home';
-import About from './app/components/About/About';
-import Projects from './app/components/Projects/Projects';
 import "../src/app/styles/App.css"
 
+const Home = lazy(() => import('./app/components/Home/Home'));
+const About = lazy(() => import('./app/components/About/About'));
+const Projects = lazy(() => import('./app/components/Projects/Projects'));
 
 const App = () => {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-          <Route path="/" element={ <Home/> }/>
-          <Route path="/about" element={ <About/> }/>
-          <Route path="/project" element={ <Projects/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Projects />} />
       </Routes>
-  )    
+    </Suspense>
+  );
 }
 
 export default App;
