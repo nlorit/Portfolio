0d2d28fbcf3b33b72bad64e8255b2c6ee71bffb9
Nathan LORIT <nathan.lorit@viacesi.fr>
2023-12-15 15:22:43 +0100

    a

--- portfolio_website/src/index.js
+++ portfolio_website/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import serviceWorker from './service-worker.js';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -20,4 +19,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker();
